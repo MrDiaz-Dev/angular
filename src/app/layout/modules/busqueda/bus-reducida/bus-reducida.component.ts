8b7536d89c74c1bab6ef4dc92590762565f5c854
Gabriel Diaz <gabrieldiazariza@gmail.com>
2024-12-03 08:19:13 -0400

    mas avances con bus reducida

--- src/app/layout/modules/busqueda/bus-reducida/bus-reducida.component.ts
+++ src/app/layout/modules/busqueda/bus-reducida/bus-reducida.component.ts
@@ -1,19 +1,54 @@
-import { Component } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PrimeNgModule } from 'src/app/layout/shared/prime-ng/prime-ng.module';
+import { CustomMessageService } from 'src/app/services/utils/message.service';
 
 @Component({
   selector: 'app-bus-reducida',
   standalone: true,
-  imports: [],
+  imports: [PrimeNgModule, ReactiveFormsModule, FormsModule],
   templateUrl: './bus-reducida.component.html',
   styleUrl: './bus-reducida.component.scss',
 })
 export class BusReducidaComponent {
   constructor() {}
 
+  // region servicios y otras dependencias
+  private fb = inject(UntypedFormBuilder)
+  private router = inject(Router)
+  private messageService = inject(CustomMessageService)
+
+  // region variables
   titulo = 'Busqueda reducida';
 
+  camposDeBusqueda = this.fb.group({
+    nombre: [null],
+    apellido1: [null],
+    apellido2: [null],
+  })
+
+  // region metodos
+
   ngOnInit() {
-    console.log('Busqueda reducida');
+    console.warn('Busqueda reducida');
+  }
+  
+  genURLParams(): string {
+    let urlParams = new HttpParams();
+    
+    for (const campo in this.camposDeBusqueda.value) {
+      if (this.camposDeBusqueda.value[campo]) {
+        urlParams = urlParams.set(campo, this.camposDeBusqueda.value[campo]);
+      }
+    }
+    
+    return urlParams.toString();
+  }
+
+  buscar() {
+    this.router.navigate(['/busqueda', this.genURLParams()]);
   }
 
   ngOnDestroy() {
