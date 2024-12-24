5032d4cb5480a20c2efe3aed3cd7c5f1b2fb2577
Gabriel Diaz <gabrieldiazariza@gmail.com>
2024-12-24 18:43:25 -0400

    feat: agregar mensajes informativos sobre la situación laboral en el componente de datos personales

--- src/app/layout/modules/personal/cabecera-datos-personales/cabecera-datos-personales.component.ts
+++ src/app/layout/modules/personal/cabecera-datos-personales/cabecera-datos-personales.component.ts
@@ -1,7 +1,8 @@
-import { Component, model } from '@angular/core';
+import { Component, computed, input, model, ViewEncapsulation } from '@angular/core';
 import { DatosPersonales } from 'src/app/layout/interfaces/datos-personales.interface';
 import { PrimeNgModule } from 'src/app/layout/shared/prime-ng/prime-ng.module';
 import { environment } from 'src/environments/environment';
+import { Message } from 'primeng/api';
 
 @Component({
   selector: 'app-cabecera-datos-personales',
@@ -9,11 +10,23 @@ import { environment } from 'src/environments/environment';
   imports: [PrimeNgModule],
   templateUrl: './cabecera-datos-personales.component.html',
   styleUrl: './cabecera-datos-personales.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CabeceraDatosPersonalesComponent {
   // #region variables
 
   datosPersonales = model<DatosPersonales>();
+
+  tituloSituacionLaboralActual = input<string>();
+
+  tituloSituacionLaboralActualPMessage = computed<Message[]>(() => {
+    return [{
+      severity: 'info',
+      summary: 'Tipo de empleado:',
+      detail: this.tituloSituacionLaboralActual(),
+      icon: 'pi pi-briefcase',
+    }];
+  })
 
   cargarFotografiaURL(): string {
     return this.datosPersonales()?.fotografia
