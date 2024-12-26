35656da613ace4f95a3b4c766852c9b8ea9b2ffd
Gabriel Diaz <gabrieldiazariza@gmail.com>
2024-12-25 22:45:12 -0400

    permisos

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UtilService {
-
-
   controlAndFormatIRIs(body: any): any {
     let newBody = structuredClone(body);
 
     for (const property in newBody) {
       if (newBody[property] != null) {
+        let newCampo;
         switch (property) {
           case 'idPais':
-            let newCampo = newBody[property].toString().includes('/')
+            newCampo = newBody[property].toString().includes('/')
               ? newBody[property]
               : '/api/paises/' + newBody[property];
+            newBody[property] = newCampo;
+            break;
+          case 'idTipo':
+            newCampo = newBody[property].toString().includes('/')
+              ? newBody[property]
+              : '/api/tipos-trabajadores/' + newBody[property];
             newBody[property] = newCampo;
             break;
           default:
@@ -40,11 +45,13 @@ export class UtilService {
         switch (property) {
           case 'telefono1':
             if (typeof newBody[property] === 'string') {
-              // convertir un string '999 999 999' en un entero 999999999 
+              // convertir un string '999 999 999' en un entero 999999999
               // o un strinf '999 9__ ___' en un entero 9999
               // o un string '99 99999999 99' en un entero 999999999999
               // o un string '99 999999__ __' en un entero 99999999
-              newBody[property] = parseInt(newBody[property].replace(/\D/g, ''));
+              newBody[property] = parseInt(
+                newBody[property].replace(/\D/g, ''),
+              );
             }
             break;
           default:
