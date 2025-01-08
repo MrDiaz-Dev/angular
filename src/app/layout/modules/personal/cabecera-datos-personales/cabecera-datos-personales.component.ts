06540dc46c05d42a62fbf8cbd790ca6810acf702
Gabriel Diaz <gabrieldiazariza@gmail.com>
2025-01-08 10:52:01 -0400

    feat: mejorar la lógica de mensajes en el componente de cabecera de datos personales y evitar la recarga constante de la cabecera al cambiar de pagina

--- src/app/layout/modules/personal/cabecera-datos-personales/cabecera-datos-personales.component.ts
+++ src/app/layout/modules/personal/cabecera-datos-personales/cabecera-datos-personales.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, inject, input, model, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  model,
+  ViewEncapsulation,
+} from '@angular/core';
 import { DatosPersonales } from 'src/app/layout/interfaces/datos-personales.interface';
 import { PrimeNgModule } from 'src/app/layout/shared/prime-ng/prime-ng.module';
 import { environment } from 'src/environments/environment';
@@ -16,7 +23,6 @@ import { FechaService } from 'src/app/services/utils/fecha.service';
   encapsulation: ViewEncapsulation.None,
 })
 export class CabeceraDatosPersonalesComponent {
-
   // region servicios y otras dependencias
   fechaService = inject(FechaService);
 
@@ -26,28 +32,75 @@ export class CabeceraDatosPersonalesComponent {
 
   datosComunesIdTipo = input();
 
+  datosComunesIdTipoMessagePrevValue: Message[] = [];
+
   bajaMedicaActual = input<string>();
 
+  bajaMedicaActualMessagePrevValue: Message[] = [];
+
   tituloSituacionLaboralActualPMessage = computed<Message[]>(() => {
-    return [{
-      severity: 'info',
-      summary: `${this.datosComunesIdTipo()['idCategoria']['nombre']}`,
-      detail: this.datosComunesIdTipo()['nombre'] == this.datosComunesIdTipo()['idCategoria']['nombre']
-        ? ''
-        : this.datosComunesIdTipo()['nombre'],
-      icon: 'pi pi-briefcase',
-    }];
-  })
+    // si no hay datos lo dejamos como estaba
+    if (!this.datosComunesIdTipo()) {
+      return this.datosComunesIdTipoMessagePrevValue;
+    }
+
+    // si hay datos contruimos el mensaje
+    let newDatosComunesIdTipoMessageValue = [
+      {
+        severity: 'info',
+        summary: `${this.datosComunesIdTipo()['idCategoria']['nombre']}`,
+        detail:
+          this.datosComunesIdTipo()['nombre'] ==
+          this.datosComunesIdTipo()['idCategoria']['nombre']
+            ? ''
+            : this.datosComunesIdTipo()['nombre'],
+        icon: 'pi pi-briefcase',
+      },
+    ];
+
+    // si el mensaje es igual al anterior no lo actualizamos y retornamos el anterior
+    if (
+      JSON.stringify(newDatosComunesIdTipoMessageValue) ===
+      JSON.stringify(this.datosComunesIdTipoMessagePrevValue)
+    ) {
+      return this.datosComunesIdTipoMessagePrevValue;
+    }
+
+    // si es diferente actualizamos el previo y retornamos el nuevo mensaje
+    this.datosComunesIdTipoMessagePrevValue = newDatosComunesIdTipoMessageValue;
+
+    return newDatosComunesIdTipoMessageValue;
+  });
 
   bajaMedicaActualPMessage = computed<Message[]>(() => {
-    if (!this.bajaMedicaActual()) return [];
-    return [{
-      severity: 'error',
-      summary: `Baja médica activa:`,
-      detail: `Desde ${this.fechaService.formatFecha(this.bajaMedicaActual())}`,
-      icon: 'pi pi-exclamation-circle',
-    }];
-  })
+    // si no hay datos lo dejamos como estaba
+    if (!this.bajaMedicaActual()) {
+      return this.bajaMedicaActualMessagePrevValue;
+    }
+
+    // si hay datos contruimos el mensaje
+    let newBajaMedicaActualMessageValue = [
+      {
+        severity: 'error',
+        summary: `Baja médica activa:`,
+        detail: `Desde ${this.fechaService.formatFecha(this.bajaMedicaActual())}`,
+        icon: 'pi pi-exclamation-circle',
+      },
+    ];
+
+    // si el mensaje es igual al anterior no lo actualizamos y retornamos el anterior
+    if (
+      JSON.stringify(newBajaMedicaActualMessageValue) ===
+      JSON.stringify(this.bajaMedicaActualMessagePrevValue)
+    ) {
+      return this.bajaMedicaActualMessagePrevValue;
+    }
+
+    // si es diferente actualizamos el previo y retornamos el nuevo mensaje
+    this.bajaMedicaActualMessagePrevValue = newBajaMedicaActualMessageValue;
+
+    return newBajaMedicaActualMessageValue;	
+  });
 
   cargarFotografiaURL(): string {
     return this.datosPersonales()?.fotografia
